4516accaaf8a7d4fc31ae852505125c01fb2be70
Salem Beiruti <726709@student.dusd.net>
2024-10-18 15:28:08 -0700

    Updated game
    Added more tile palette options, fixed tile size, and horizontal character movement using left and right arrow keys.

--- 2DBeginnerAdventureP8Beiruti/Assets/Scripts/PlayerController.cs
+++ 2DBeginnerAdventureP8Beiruti/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
@@ -13,8 +14,22 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float horizontal = 0.0f;
+        if (Keyboard.current.leftArrowKey.isPressed)
+        {
+            horizontal = -1.0f;
+        }
+        else if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            horizontal = 1.0f;
+        }
+        Debug.Log(horizontal);
+
+
+
+
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        position.x = position.x + 0.1f * horizontal;
         transform.position = position;
     }
 }
