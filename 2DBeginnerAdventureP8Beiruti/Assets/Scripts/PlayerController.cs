d82ae4df5ce95a8a83e2cf7b5acaf712732735e2
Salem Beiruti <726709@student.dusd.net>
2024-11-01 15:27:45 -0700

    Collectables/ Damage Zones & Enemies
    Added a max hp of 5, made collectables that increase your hp when collected, added damage zones that decrease your health, and moving enemies that also decrease your health. Added onto map.

--- 2DBeginnerAdventureP8Beiruti/Assets/Scripts/PlayerController.cs
+++ 2DBeginnerAdventureP8Beiruti/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,16 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 3.0f;
+
+    public int maxHealth = 5;
+    public float timeInvincible = 2;
+    public int health { get { return currentHealth; } }
+    int currentHealth;
+
+    bool isInvincible;
+    float invincibleTimer;
+
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
@@ -15,7 +25,7 @@ public class PlayerController : MonoBehaviour
         //QualitySettings.vSyncCount = 0;
         //Application.targetFrameRate = 60;
         rigidbody2d = GetComponent<Rigidbody2D>();
-
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -23,14 +33,37 @@ public class PlayerController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if(invincibleTimer < 0 )
+            {
+                isInvincible = false;
+            }
+        }
     }
     void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
-        position.x = position.x + 10.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 10.0f * vertical * Time.deltaTime; ;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime; ;
 
         rigidbody2d.MovePosition(position);
+    }
+    public void ChangeHealth(int amount)
+    {
+        if(amount < 0)
+        {
+            if(isInvincible)
+            {
+                return;
+            }
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
 
