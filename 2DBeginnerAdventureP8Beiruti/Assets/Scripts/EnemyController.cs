f92cd95bbad69ed5719b71c75334c0e2f901511c
Salem Beiruti <726709@student.dusd.net>
2024-11-14 15:28:06 -0800

    Ui and Particles
    Added uihealthbar that changes depending on hp. also added smoke particles that come from enemies and disappear when enemies are fixed

--- 2DBeginnerAdventureP8Beiruti/Assets/Scripts/EnemyController.cs
+++ 2DBeginnerAdventureP8Beiruti/Assets/Scripts/EnemyController.cs
@@ -10,6 +10,8 @@ public class EnemyController : MonoBehaviour
     public bool vertical;
     public float changeTime = 3.0f;
 
+    public ParticleSystem smokeEffect;
+
     Rigidbody2D rigidbody2d;
 
     bool broken = true;
@@ -81,5 +83,6 @@ public class EnemyController : MonoBehaviour
         broken = false;
         rigidbody2d.simulated = false;
         animator.SetTrigger("Fixed");
+        smokeEffect.Stop();
     }
 }
